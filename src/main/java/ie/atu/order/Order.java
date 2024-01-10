e59322e17fd3a2644a1f77b914f5bca508b0682c
Olamide Aderogba <G00401971@atu.ie>
2024-01-10 10:15:00 +0000

    Created Order Class for project

---
+++ src/main/java/ie/atu/order/Order.java
@@ -0,0 +1,16 @@
+package ie.atu.order;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order {
+    private String orderId;
+    private String productId;
+    private int quantity;
+    private String customerId;
+
+}
